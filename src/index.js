967b2922f5cb6c8a75519ec9a993f191645a5ad8
Mayank Kamboj <mayankkamboj47@gmail.com>
2024-04-20 08:46:08 +0000

    step 09

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ const {parseQuery} = require('./queryParser');
 const readCSV = require('./csvReader');
 
 async function executeSELECTQuery(query) {
-    const { fields, table, whereClauses, joinTable, joinCondition } = parseQuery(query);
+    const { fields, table, whereClauses, joinTable, joinCondition, joinType } = parseQuery(query);
     let data = await readCSV(`${table}.csv`);
-    data = await join(data, joinTable, joinCondition, fields, table);
+    data = await join(data, joinTable, joinCondition, fields, table, joinType);
     const filteredData = data.filter(row => whereClauses.every(clause => evaluateCondition(row, clause)))
     return select(filteredData, fields);
 }
@@ -35,24 +35,28 @@ function evaluateCondition(row, clause) {
 async function join(data, joinTable, joinCondition, fields, table, type="INNER") {
     if (!joinTable || !joinCondition) return data;
     const joinData = await readCSV(`${joinTable}.csv`);
+    if(type==="RIGHT") return join(joinData, table, joinCondition, fields, joinTable, "LEFT");
     const result = [];
-    const [leftField, rightField] = getJoinFields(joinCondition);
+    const [leftField, rightField] = getJoinFields(joinCondition, table, joinTable);
     for (const mainRow of data) {
         const mainValue = mainRow[leftField];
+        let atLeastOne = false;
         for (const joinRow of joinData) {
             const joinValue = joinRow[rightField];
             if (mainValue === joinValue) {
                 result.push(buildRow(mainRow, joinRow, table, fields));
+                atLeastOne = true;
             }
         }
+        if(!atLeastOne && type === "LEFT") result.push(buildRow(mainRow, {}, table, fields));
     }
     return result;
 }
 
-function getJoinFields(joinCondition) {
-    const left = joinCondition.left.split('.')[1];
-    const right = joinCondition.right.split('.')[1];
-    return [left, right];
+function getJoinFields(joinCondition, table1, table2) {
+    const [t1, v1] = joinCondition.left.split('.');
+    const [t2, v2] = joinCondition.right.split('.');
+    return table1 == t1 && table2 == t2 ? [v1, v2] : [v2, v1];
 }
 
 function buildRow(mainRow, joinRow, table, fields) {
@@ -60,7 +64,7 @@ function buildRow(mainRow, joinRow, table, fields) {
 
     for (const field of fields) {
         const [tableName, fieldName] = field.split('.');
-        row[field] = tableName === table ? mainRow[fieldName] : joinRow[fieldName];
+        row[field] = (tableName === table ? mainRow[fieldName] : joinRow[fieldName]) || null;
     }
 
     return row;
